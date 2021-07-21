ea32ac0d9303896ec72755bd79eea9945b67de68
Younyeojin <yjyoon2107@gmail.com>
2021-07-21 12:16:32 +0900

    6

--- scraping/bugmusic.py
+++ scraping/bugmusic.py
@@ -1,12 +1,13 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen                 # import 할때 소문자 = 함수
+import urllib.parse
 
 '''
 지원하는 Parser 종류
 "html.parser" : 빠르지만 유연하지 않기 때문에 단순한 HTML문서에 사용합니다.    ②순위
-"lxml" : 매우 빠르고 유연합니다.    ①순위; 제일 빠름
+"lxml" : 매우 빠르고 유연합니다.                                          ①순위; 제일 빠름
 "xml" : XML 파일에만 사용합니다.
-"html5lib" : 복잡한 구조의 HTML에 대해서 사용합니다.     ②순위
+"html5lib" : 복잡한 구조의 HTML에 대해서 사용합니다.                        ②순위
 '''
 
 
@@ -17,19 +18,60 @@ class Bugmusic(object):
 
     def scrap(self):
         soup = BeautifulSoup(urlopen(self.url), 'lxml')                   # 메소드    / None: null이랑 같음
-        n_artists = 0
-        n_title = 0
-        ls = soup.find_all(name='p', attrs={'class':'artist'})
-        ls2 = soup.find_all(name='p', attrs={'class':'title'})
-        print(f'List size is {len(ls)}')
-        for i in ls:                                                      # name='p' : 'p'=태그
-            n_artists += 1
+        # n_artists = 0
+        # n_title = 0
+        _ = 0
+        artists = soup.find_all(name='p', attrs={'class':'artist'})
+        titles = soup.find_all(name='p', attrs={'class':'title'})
+        print(f'List size is {len(artists)}')
+        for i, j in zip(artists, titles):
+            _ += 1
+            print(f"Rank {str(_)} Artist: {i.find('a').text}, Title: {j.find('a').text}")
+
+
+class MelonMusic(object):
+    def __init__(self, url):
+        self.url = url
+        self.headers = {'User-Agent':'Mozilla/5.0'}
+
+    def scrap(self):
+        x = urllib.request.Request(self.url, headers=self.headers)
+        soup = BeautifulSoup(urllib.request.urlopen(x).read(), 'lxml')
+        _ = 0
+        artists = soup.find_all(name='div', attrs={'class': 'ellipsis rank02'})
+        titles = soup.find_all(name='div', attrs={'class': 'ellipsis rank01'})
+        print(f'List size is {len(artists)}')
+        for i, j in zip(artists, titles):
+            _ += 1
+            print(f"Rank {str(_)} Artist: {i.find('a').text}, Title: {j.find('a').text}")
+
+def main():
+    # 20210720
+    # 16
+    '''     Bugmusic(f'https://music.bugs.co.kr/chart/track/realtime/total?'
+                     f'chartdate={input("Input Date")}&charthour={input("Input Hour")}').scrap()   '''
+
+    Bugmusic(f'https://music.bugs.co.kr/chart/track/realtime/total?'
+             f'chartdate={"20210721"}&charthour={"09"}').scrap()
+
+
+if __name__ == '__main__':
+    MelonMusic(f'https://www.melon.com/chart/index.htm?dayTime=2021072016').scrap()
+
+
+
+
+'''
+        for i, j in enumerate(artists):                                 # name='p' : 'p'=태그
+            _ += 1
+            print(f"Rank {str(_)}Artist: {j.find('a').text}, Title: {titles[i].find('a').text}")
+         n_artists += 1
             print(str(n_artists) + " Rank")
             # print(str(n_title)+"Name")
             print("Artist : "+ i.find('a').text+' - '+ls2[n_title].find('a').text)
             n_title += 1
         # print("*"*100)
-        '''ls = soup.find_all(name='p', attrs={'class':'title'})
+        ls = soup.find_all(name='p', attrs={'class':'title'})
         print(f'List size is {len(ls)}')
         for i in ls:
             n_title += 1
@@ -38,16 +80,3 @@ class Bugmusic(object):
         # print(f'{self.url}')
 
 
-def main():
-    # 20210720
-    # 16
-    '''     Bugmusic(f'https://music.bugs.co.kr/chart/track/realtime/total?'
-                     f'chartdate={input("Input Date")}&charthour={input("Input Hour")}').scrap()   '''
-
-
-    Bugmusic(f'https://music.bugs.co.kr/chart/track/realtime/total?'
-             f'chartdate={"20210720"}&charthour={"16"}').scrap()
-
-
-if __name__ == '__main__':
-    main()
--- scraping/melonmusic.py
+++ scraping/melonmusic.py
@@ -1,17 +1,28 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-class MelonMusic(object):
-    def __init__(self):
-        pass
 
-    
+class MelonMusic(object):
+    def __init__(self, url):
+        self.url = url
+
     def scrap(self):
-        pass
+        soup = BeautifulSoup(urlopen(self.url), 'lxml')
+        n_artists = 0
+        n_title = 0
+        ls = soup.find_all(name='div', attrs={'class':'artist'})
+        ls2 = soup.find_all(name='div', attrs={'class':'artist'})
+        print(f'Top {len(ls)}')
+        for i in ls:
+            n_artists += 1
+            print(str(n_artists)+"Rank")
+            print("Artist: " + i.find('a').text + ' - '+ls2[n_title].find('a').text)
+            n_title += 1
 
 
 def main():
-    pass
+    MelonMusic(f'https://www.melon.com/chart/index.htm?'
+               'dayTime=2021072109').scrap()
 
 
 if __name__ == '__main__':
