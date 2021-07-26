d173b51164ad4001f5ad2c330657c18200b874ce
Younyeojin <yjyoon2107@gmail.com>
2021-07-26 11:58:16 +0900

    15

---
+++ modu/template/__init__.py
@@ -0,0 +1,22 @@
+from common.menu import print_menu
+from modu.template.basic_plot import plot_show, plot_two_list_show, plot_legend, plot_change_color, plot_line_style, \
+    scatter
+
+if __name__ == '__main__':
+    while 1:
+        menu = print_menu(['Exit', 'plot Show', 'plot_two_list_show', 'plot_legend', 'plot_change_color', 'plot_line_style',
+                           'scatter', 'Dataframe to CSV'])
+        if menu == 0:
+            break
+        elif menu == 1:
+            plot_show()
+        elif menu == 2:
+            plot_two_list_show()
+        elif menu == 3:
+            plot_legend()
+        elif menu == 4:
+            plot_change_color()
+        elif menu == 5:
+            plot_line_style()
+        elif menu == 6:
+            scatter()
--- titanic/template/plot.py
+++
@@ -1,9 +0,0 @@
-from titanic.model.dataset import Dataset
-from titanic.model.service import Service
-import matplotlib.pyplot as plt
-
-
-class Plot(object):
-    service = Service()
-    dataset = Dataset()
-
